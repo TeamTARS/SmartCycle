bf771a08d1c0b1be65b8e64e7fa6ec744953564f
MMoudy49 <mmoudy49@gmail.com>
2020-02-28 11:16:24 -0800

    Formatted all documents using prettier

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # SmartCycle
+
 Team Tars project repository including both client-side and server-side code.
 
 ## Basic commands
+
 ### Development
+
 `npm run client` to start dev server (listening on port `3000`) for client-side code.
 
 `npm run server` to start dev server (listening on port `8080`) for server-side code.
@@ -10,6 +13,7 @@ Team Tars project repository including both client-side and server-side code.
 `npm run dev` runs both client and server at the same time.
 
 ### Production
+
 `npm run build` to make production build on client-side code in webpack-build folder and served by the server.
 
 `npm run start` to make production build and then start the server.
--- webpack.config.js
+++ webpack.config.js
@@ -5,70 +5,70 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const DIST_DIR = "webpack-build";
 
 module.exports = {
-	entry: "./src/client/index.tsx",
-	output: {
-		publicPath: "/",
-		filename: "[name].js",
-		chunkFilename: "[name].chunk.js",
-		path: path.join(__dirname, DIST_DIR)
-	},
-	devtool: "source-map",
-	resolveLoader: {
-		modules: [path.join(__dirname, "node_modules")]
-	},
-	module: {
-		rules: [
-			// Style loaders
-			{ test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"] },
-			{ test: /\.css$/, use: ["style-loader", "css-loader"] },
-			{ test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] },
-			{ test: /\.(woff|woff2|eot|ttf)$/, loader: "url-loader?limit=100000" },
-			// All files with a '.ts' or '.tsx' extension will be ts-linted
-			{
-				test: /\.(ts|tsx|properties)$/,
-				enforce: "pre",
-				use: [
-					{
-						options: {
-							eslintPath: require.resolve("eslint")
-						},
-						loader: require.resolve("eslint-loader")
-					}
-				],
-				exclude: /node_modules/
-			},
-			// TS loader
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/
-			},
-			// JS loader
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader"
-				}
-			}
-		]
-	},
-	resolve: {
-		extensions: [".js", ".jsx", ".ts", ".tsx"]
-	},
-	devServer: {
-		contentBase: path.join(__dirname, DIST_DIR),
-		compress: true,
-		port: 3000,
+  entry: "./src/client/index.tsx",
+  output: {
+    publicPath: "/",
+    filename: "[name].js",
+    chunkFilename: "[name].chunk.js",
+    path: path.join(__dirname, DIST_DIR)
+  },
+  devtool: "source-map",
+  resolveLoader: {
+    modules: [path.join(__dirname, "node_modules")]
+  },
+  module: {
+    rules: [
+      // Style loaders
+      { test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"] },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] },
+      { test: /\.(woff|woff2|eot|ttf)$/, loader: "url-loader?limit=100000" },
+      // All files with a '.ts' or '.tsx' extension will be ts-linted
+      {
+        test: /\.(ts|tsx|properties)$/,
+        enforce: "pre",
+        use: [
+          {
+            options: {
+              eslintPath: require.resolve("eslint")
+            },
+            loader: require.resolve("eslint-loader")
+          }
+        ],
+        exclude: /node_modules/
+      },
+      // TS loader
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      // JS loader
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, DIST_DIR),
+    compress: true,
+    port: 3000,
 
-		// Allow access to dev proxy from external boxes
-		host: "0.0.0.0",
-		disableHostCheck: true
-	},
-	plugins: [
-		new CleanWebpackPlugin(), // Defaults to cleaning output.path
-		new HtmlWebpackPlugin({
-			template: "./public/index.html"
-		})
-	]
+    // Allow access to dev proxy from external boxes
+    host: "0.0.0.0",
+    disableHostCheck: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Defaults to cleaning output.path
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
 };
