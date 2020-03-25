1c0aa0a15179401c3afee838809c22a0f1eb90af
MMoudy49 <mmoudy49@gmail.com>
2020-03-25 16:05:05 -0500

    Updated alert to be more dynamic

--- src/client/Pages/CommonComponents/Camera/CameraView.tsx
+++ src/client/Pages/CommonComponents/Camera/CameraView.tsx
@@ -31,7 +31,10 @@ const CameraView = (props: { openToastMessage: any }) => {
   const setPredictionHelper = (binInfo: { itemName: string; bin: string }) => {
     setPrediction(binInfo);
     if (binInfo.itemName.includes("bottle")) {
-      props.openToastMessage();
+      props.openToastMessage(
+        "Message from CSO: Using reusable glass could help protecting the environment!",
+        "info"
+      );
     }
   };
 
@@ -123,7 +126,11 @@ const CameraView = (props: { openToastMessage: any }) => {
         </div>
       </Grid>
       <Grid item>
-        <ReportDataPopup itemName={prediction.itemName} bin={prediction.bin} />
+        <ReportDataPopup
+          openToastMessage={props.openToastMessage}
+          itemName={prediction.itemName}
+          bin={prediction.bin}
+        />
       </Grid>
     </Grid>
   );
